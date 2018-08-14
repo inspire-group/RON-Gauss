c4ff66e67613f7c2e6548b42f453f3fbd7b60704
tchanyaswad <tc7@princeton.edu>
2018-08-14 18:18:17 +0700

    Add reconstruction and mean adjustment option

--- ron_gauss.py
+++ ron_gauss.py
@@ -4,6 +4,15 @@
 #
 # Version 1.0
 #	- Initial implementation.
+#
+# Version 1.1
+#	- Bug fixes.
+#	- Add reconstruction option.
+#	- Minor stylistic update.
+#
+# Version 1.2
+#	- Add mean adjustment option for unsupervised and supervised algs.
+#
 #---------------------------
 
 import numpy as np
@@ -18,38 +27,59 @@ class RON_Gauss:
 		self.epsilonCov = epsilonCov
 	
 	
-	def generate_dpdata(self, X, dimension, y=None, maxY=None):
-		prng = np.random.RandomState()
+	def generate_dpdata(self, X, dimension, y=None, numSamples=None, maxY=None, reconstruct=False, meanAdjusted=False, seed=7):
+		prng = np.random.RandomState(seed)
 		if self.algorithm == 'unsupervised':
-			(Xbar,_) = self.data_preprocessing(X,self.epsilonMean)
-			(Xred,_) = self.ron_projection(Xbar,dimension)
+			(Xbar,muPriv) = self._data_preprocessing(X,self.epsilonMean)
+			(Xred,onProj) = self._ron_projection(Xbar,dimension)
 			
 			(N,P) = Xred.shape
 			b = (2.*np.sqrt(P))/(N*self.epsilonCov)
-			
+			if numSamples is None:
+				numSam = N
+			else:
+				numSam = numSamples
 			scatMat = np.inner(Xred.T,Xred.T)/N
 			lapNoise = np.random.laplace(scale=b,size=(P,P))
 			dpCov = scatMat + lapNoise
-			synthData = prng.multivariate_normal(np.zeros(P),dpCov, N)
+			synthData = prng.multivariate_normal(np.zeros(P),dpCov, numSam)
 			dpX = synthData
 			dpY = None
+			if reconstruct:
+				dpX = self._reconstruction(dpX,onProj)
+			else:
+				muPriv = np.inner(muPriv,onProj)
+			
+			if meanAdjusted:
+				dpX = dpX + muPriv
+			
 		
 		elif self.algorithm == 'supervised':
-			(Xbar,_) = self.data_preprocessing(X,self.epsilonMean)
-			(Xred,_) = self.ron_projection(Xbar,dimension)
+			(Xbar,muPriv) = self._data_preprocessing(X,self.epsilonMean)
+			(Xred,onProj) = self._ron_projection(Xbar,dimension)
 			
 			(N,P) = Xred.shape
 			b = (2.*np.sqrt(P) + 4.*np.sqrt(P)*maxY + maxY**2)/(N*self.epsilonCov)
-			
+			if numSamples is None:
+				numSam = N
+			else:
+				numSam = numSamples		
 			yReshaped = y.reshape(len(y),1)
 			augMat = np.hstack((Xred,yReshaped))
 			scatMat = np.inner(augMat.T,augMat.T)/N
 			lapNoise = np.random.laplace(scale=b,size=(P+1,P+1))
 			dpCov = scatMat + lapNoise
 	
-			synthData = prng.multivariate_normal(np.zeros(P + 1), dpCov, N)
+			synthData = prng.multivariate_normal(np.zeros(P + 1), dpCov, numSam)
 			dpX = synthData[:,0:-1]
 			dpY = synthData[:,-1]
+			if reconstruct:
+				dpX = self._reconstruction(dpX,onProj)
+			else:
+				muPriv = np.inner(muPriv,onProj)
+			
+			if meanAdjusted:
+				dpX = dpX + muPriv
 		
 		elif self.algorithm == 'gmm':
 			synX = np.array([])
@@ -57,30 +87,37 @@ class RON_Gauss:
 			for lab in np.unique(y):
 				idx = np.where(y == lab)
 				xClass = X[idx]
-				(Xbar, muPriv) = self.data_preprocessing(xClass,self.epsilonMean)
-				(Xred, onProj) = self.ron_projection(Xbar,dimension)
+				(Xbar, muPriv) = self._data_preprocessing(xClass,self.epsilonMean)
+				(Xred, onProj) = self._ron_projection(Xbar,dimension)
 				
 				(N,P) = Xred.shape
 				b = (2.*np.sqrt(P))/(N*self.epsilonCov)
+				if numSamples is None:
+					numSam = N
+				else:
+					numSam = numSamples
 				muRed = np.inner(muPriv,onProj)
 				scatMat = np.inner(Xred.T,Xred.T)/N
 				lapNoise = np.random.laplace(scale=b,size=(P,P))
 				dpCov = scatMat + lapNoise
-				synthData = prng.multivariate_normal(muRed,dpCov, N)
+				synthData = prng.multivariate_normal(muRed,dpCov, numSam)
+				if reconstruct:
+					synthData = self._reconstruction(synthData,onProj)
 				if len(synX) == 0:
 					synX = synthData
 				else:
 					synX = np.vstack((synX,synthData))
 				
-				synY = np.append(synY,lab*np.ones(N))
+				synY = np.append(synY,lab*np.ones(numSam))
 			dpX = synX
 			dpY = synY
+
 			
 		return (dpX,dpY)
 		
 	
 	
-	def data_preprocessing(self, X, epsMu):
+	def _data_preprocessing(self, X, epsMu):
 		(N,M) = X.shape
 		#pre-normalize
 		Xscaled = preprocessing.normalize(X)
@@ -95,15 +132,19 @@ class RON_Gauss:
 		Xbar = preprocessing.normalize(Xbar)
 		return Xbar, muPriv
 	
-	def ron_projection(self,Xbar,dim):
+	def _ron_projection(self,Xbar,dim):
 		(N,M) = Xbar.shape
-		randProj = self.generate_rand_onproj(M)
-		onProj = randProj[0:dim]
+		randProj = self._generate_rand_onproj(M)
+		onProj = randProj[0:dim] #take the rows
 		Xred = np.inner(Xbar,onProj)
 		return Xred, onProj
 		
 	
-	def generate_rand_onproj(self,m):
+	def _reconstruction(self, Xproj, onProj):
+		Xrecon = np.inner(Xproj,onProj.T)
+		return Xrecon
+	
+	def _generate_rand_onproj(self,m):
 		#generate random matrix
 		randMat = np.random.uniform(size=(m,m))
 		# QR factorization
